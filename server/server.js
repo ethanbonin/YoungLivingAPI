4559b3623d0858606163a7551ae215908799a240
ethan <ethanbonin@gmail.com>
2017-10-06 18:40:43 -0500

    Hello world

--- server/server.js
+++ server/server.js
@@ -226,7 +226,6 @@ is_retail_customer = function(retail_customers, all_members) {
 };
 
 //This only runs when on production
-if (process.env.NODE_ENV === 'production'){
   //Express will serve  up production assets
   //like our main.js file. or main.css file
   const path = require('path');
@@ -240,7 +239,6 @@ if (process.env.NODE_ENV === 'production'){
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-};
 
 app.listen(_PORT, () => {
   console.log("Server is Running on port: " + _PORT);
