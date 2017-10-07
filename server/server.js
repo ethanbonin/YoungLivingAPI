1339f2ea1298f89f56adedbef323d9d1a4d34e15
ethan <ethanbonin@gmail.com>
2017-10-06 20:14:36 -0500

    Changed back the path join thingy

--- server/server.js
+++ server/server.js
@@ -244,7 +244,7 @@ if (process.env.NODE_ENV === 'production'){
   // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname,'/client/build/index.html'));
   });
 };
 
