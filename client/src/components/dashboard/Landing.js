e3de9803228a360d11762a7edb58f5cc9095501e
Ethan Bonin <ethanbonin@gmail.com>
2017-11-01 08:53:24 -0600

    Reduced the amount of code in the landing page in one function

--- client/src/components/dashboard/Landing.js
+++ client/src/components/dashboard/Landing.js
@@ -18,47 +18,115 @@ const cover_image = require("../../assets/cover_image.jpg");
 export default class HomepageLayout extends Component {
   state = {};
 
+  renderCoverImage() {
+    return (
+      <Segment inverted style={{ padding: "0em 0em" }} vertical>
+        <Image src={cover_image} className="coverImage" />
+        <p className="coverText">
+          We are here to help grow your business with YoungLiving.
+        </p>
+      </Segment>
+    );
+  }
+
+  introDescription() {
+    return (
+      <Grid.Row>
+        <Grid.Column width={8}>
+          <Header as="h3" style={{ fontSize: "2em" }}>
+            We Help Companies and Companions
+          </Header>
+          <p style={{ fontSize: "1.33em" }}>
+            We can give your company superpowers to do things that they never
+            thought possible. Let us delight your customers and empower your
+            needs... through pure data analytics.
+          </p>
+          <Header as="h3" style={{ fontSize: "2em" }}>
+            We Make Bananas That Can Dance
+          </Header>
+          <p style={{ fontSize: "1.33em" }}>
+            Yes that's right, you thought it was the stuff of dreams, but even
+            bananas can be bioengineered.
+          </p>
+        </Grid.Column>
+        <Grid.Column floated="right" width={6}>
+          <Image bordered size="large" src={image} />
+        </Grid.Column>
+      </Grid.Row>
+    );
+  }
+
+  reviewsDescription() {
+    return (
+      <Grid.Row textAlign="center">
+        <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
+          <Header as="h3" style={{ fontSize: "2em" }}>
+            "What a Company"
+          </Header>
+          <p style={{ fontSize: "1.33em" }}>
+            That is what they all say about us
+          </p>
+        </Grid.Column>
+        <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
+          <Header as="h3" style={{ fontSize: "2em" }}>
+            "I shouldn't have gone with their competitor."
+          </Header>
+          <p style={{ fontSize: "1.33em" }}>
+            <Image avatar src="/assets/images/avatar/large/nan.jpg" />
+            <b>Nan</b> Chief Fun Officer Acme Toys
+          </p>
+        </Grid.Column>
+      </Grid.Row>
+    );
+  }
+
+
+  finalDescription(){
+    return(
+      <Container text>
+        <Header as="h3" style={{ fontSize: "2em" }}>
+          Breaking The Grid, Grabs Your Attention
+        </Header>
+        <p style={{ fontSize: "1.33em" }}>
+          Instead of focusing on content creation and hard work, we have
+          learned how to master the art of doing nothing by providing
+          massive amounts of whitespace and generic content that can seem
+          massive, monolithic and worth your attention.
+        </p>
+        <Button as="a" size="large">
+          Read More
+        </Button>
+        <Divider
+          as="h4"
+          className="header"
+          horizontal
+          style={{ margin: "3em 0em", textTransform: "uppercase" }}
+        >
+          <a>Case Studies</a>
+        </Divider>
+        <Header as="h3" style={{ fontSize: "2em" }}>
+          Did We Tell You About Our Bananas?
+        </Header>
+        <p style={{ fontSize: "1.33em" }}>
+          Yes I know you probably disregarded the earlier boasts as
+          non-sequitur filler content, but it's really true. It took years
+          of gene splicing and combinatory DNA research, but our bananas can
+          really dance.
+        </p>
+        <Button as="a" size="large">
+          I'm Still Quite Interested
+        </Button>
+      </Container>
+    )
+  }
+
   render() {
     return (
       <div>
-        <Segment
-          inverted
-          style={{ padding: "0em 0em" }}
-          vertical
-        >
-
-            <Image src={cover_image} className="coverImage" />
-            <p className="coverText">
-              We are here to help grow your business with YoungLiving.
-            </p>
-
-
-        </Segment>
-
+        {this.renderCoverImage()}
         <Segment style={{ padding: "8em 0em" }} vertical>
           <Grid container stackable verticalAlign="middle">
-            <Grid.Row>
-              <Grid.Column width={8}>
-                <Header as="h3" style={{ fontSize: "2em" }}>
-                  We Help Companies and Companions
-                </Header>
-                <p style={{ fontSize: "1.33em" }}>
-                  We can give your company superpowers to do things that they
-                  never thought possible. Let us delight your customers and
-                  empower your needs... through pure data analytics.
-                </p>
-                <Header as="h3" style={{ fontSize: "2em" }}>
-                  We Make Bananas That Can Dance
-                </Header>
-                <p style={{ fontSize: "1.33em" }}>
-                  Yes that's right, you thought it was the stuff of dreams, but
-                  even bananas can be bioengineered.
-                </p>
-              </Grid.Column>
-              <Grid.Column floated="right" width={6}>
-                <Image bordered size="large" src={image} />
-              </Grid.Column>
-            </Grid.Row>
+            {this.introDescription()}
             <Grid.Row>
               <Grid.Column textAlign="center">
                 <Button size="huge">Check Them Out</Button>
@@ -68,62 +136,11 @@ export default class HomepageLayout extends Component {
         </Segment>
         <Segment style={{ padding: "0em" }} vertical>
           <Grid celled="internally" columns="equal" stackable>
-            <Grid.Row textAlign="center">
-              <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-                <Header as="h3" style={{ fontSize: "2em" }}>
-                  "What a Company"
-                </Header>
-                <p style={{ fontSize: "1.33em" }}>
-                  That is what they all say about us
-                </p>
-              </Grid.Column>
-              <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-                <Header as="h3" style={{ fontSize: "2em" }}>
-                  "I shouldn't have gone with their competitor."
-                </Header>
-                <p style={{ fontSize: "1.33em" }}>
-                  <Image avatar src="/assets/images/avatar/large/nan.jpg" />
-                  <b>Nan</b> Chief Fun Officer Acme Toys
-                </p>
-              </Grid.Column>
-            </Grid.Row>
+            {this.reviewsDescription()}
           </Grid>
         </Segment>
         <Segment style={{ padding: "8em 0em" }} vertical>
-          <Container text>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              Breaking The Grid, Grabs Your Attention
-            </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              Instead of focusing on content creation and hard work, we have
-              learned how to master the art of doing nothing by providing
-              massive amounts of whitespace and generic content that can seem
-              massive, monolithic and worth your attention.
-            </p>
-            <Button as="a" size="large">
-              Read More
-            </Button>
-            <Divider
-              as="h4"
-              className="header"
-              horizontal
-              style={{ margin: "3em 0em", textTransform: "uppercase" }}
-            >
-              <a>Case Studies</a>
-            </Divider>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              Did We Tell You About Our Bananas?
-            </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              Yes I know you probably disregarded the earlier boasts as
-              non-sequitur filler content, but it's really true. It took years
-              of gene splicing and combinatory DNA research, but our bananas can
-              really dance.
-            </p>
-            <Button as="a" size="large">
-              I'm Still Quite Interested
-            </Button>
-          </Container>
+          {this.finalDescription()}
         </Segment>
         <Segment inverted vertical style={{ padding: "5em 0em" }}>
           <Container>
