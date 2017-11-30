d76fc12e931b5d1f769963b993add1951c952452
Ethan Bonin <ethanbonin@gmail.com>
2017-11-30 11:24:08 -0700

    Got it to reformat itself.

--- client/src/reducers/LabelsReducer.js
+++ client/src/reducers/LabelsReducer.js
@@ -4,10 +4,12 @@ export default function(state = null, action) {
   switch (action.type) {
     case FETCH_LABELS:
       console.log("FETCH_LABELS", action.payload);
+      state = action.payload;
       return action.payload;
     case UPDATE_LABELS:
-      console.log("UPDATING_LABELS", action.payload);
-      return action.payload;
+      console.log("THe state", state);
+      console.log("Update,", {prospectLabels: [action.payload]})
+      return {prospectlabels: [action.payload]};
     default:
       return state;
   }
